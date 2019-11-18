e7e19260e08be4fdff92db2e1cc69826d82900e3
ScottGarryFoster <scott@posm.co.uk>
2019-11-18 23:46:11 +0000

    Added alot
    Player works, using prefab scripts.
    
    Camera is working rather well. Camera doesn't get trapped easily on obsticles. Still need to figure out the camera going over the top and being able to be under the ground.

---
+++ Project-RPG/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraOrbit;
+    public Transform target;
+
+    private InputCameraOrbit inputCO;
+
+    void Start()
+    {
+        cameraOrbit.position = target.position;
+        inputCO = cameraOrbit.GetComponent<InputCameraOrbit>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+
+        transform.LookAt(target.position);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.tag == "Floor")
+            inputCO.SimulateMouse(0, -0.1f);
+                //transform.Rotate(Vector3.up, -1 * Time.deltaTime);
+                //cameraOrbit.transform.rotation = Quaternion.Euler(0, 0, cameraOrbit.transform.rotation.z + 1f);
+
+            //cameraOrbit.transform.position = new Vector3(cameraOrbit.transform.position.x, cameraOrbit.transform.position.y + 0.1f, cameraOrbit.transform.position.z);
+    }
+}
---
+++ Project-RPG/Assets/InputCameraOrbit.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCameraOrbit : MonoBehaviour
+{
+    public GameObject cameraOrbit;
+    public GameObject player;
+    public Camera cam;
+
+    public float rotateSpeed = 8f;
+
+    private Vector3 originalScale;
+    private float scrollAmount = 0;
+    private float seePlayerZoom = 0;
+    private float lastPlayerSeeRefresh = 0;
+
+    void Start()
+    {
+        originalScale = cameraOrbit.transform.localScale;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float h = rotateSpeed * Input.GetAxis("Mouse X");
+            float v = rotateSpeed * Input.GetAxis("Mouse Y");
+
+            SimulateMouse(h, v);
+        }
+
+        float scrollFactor = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollFactor != 0)
+            scrollAmount += scrollFactor;
+
+        cameraOrbit.transform.localScale = originalScale;
+        //else
+        //    seePlayerZoom = 0;
+
+        if (scrollAmount != 0 || seePlayerZoom != 0)
+            cameraOrbit.transform.localScale = cameraOrbit.transform.localScale * (1f - (scrollAmount + seePlayerZoom));
+
+        if (!CanSeePlayer())
+        {
+            if ((scrollAmount + seePlayerZoom) < 0.7f)
+            {
+                seePlayerZoom += 0.1f;
+            }
+        }
+    }
+
+    public void SimulateMouse(float h, float v)
+    {
+        if (cameraOrbit.transform.eulerAngles.z + v <= 0.1f || cameraOrbit.transform.eulerAngles.z + v >= 179.9f)
+            v = 0;
+
+        cameraOrbit.transform.eulerAngles = new Vector3(cameraOrbit.transform.eulerAngles.x, cameraOrbit.transform.eulerAngles.y + h, cameraOrbit.transform.eulerAngles.z + v);
+    }
+
+    bool CanSeePlayer()
+    {
+        if (CastRay(0.5f, 0.5f)) return true;
+        for(int i = 0; i < 5; i++)
+        {
+            Vector2 v2 = new Vector2(0.45f + (1 - i), 0.45f + (1 - i));
+            if (CastRay(v2.x, v2.y)) return true;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Vector2 v2 = new Vector2(0.5f + (1 - i), 0.45f + (1 - i));
+            if (CastRay(v2.x, v2.y)) return true;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Vector2 v2 = new Vector2(0.45f + (1 - i), 0.5f + (1 - i));
+            if (CastRay(v2.x, v2.y)) return true;
+        }
+        for (int i = 0; i < 5; i++)
+        {
+            Vector2 v2 = new Vector2(0.5f + (1 - i), 0.5f + (1 - i));
+            if (CastRay(v2.x, v2.y)) return true;
+        }
+        //Debug.Log("CAN NOT SEE PLAYER");
+        return false;
+    }
+
+    bool CastRay(float x, float y, bool debug = false)
+    {
+        RaycastHit hit;
+        Ray ray = cam.ViewportPointToRay(new Vector3(x, y, 0.5f));
+        if(debug)
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            if (debug)
+                Debug.Log("Hit: objectHit:" + objectHit.gameObject.name);
+
+            if (objectHit.name == player.name) return true;
+            
+            // Do something with the object that was hit by the raycast.
+        }
+        return false;
+    }
+}
